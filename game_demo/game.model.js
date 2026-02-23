18efe8bf31f611cd244bdcc46e98b6beb275abac
KitsuneNeoxy <1stXPoorinut@gmail.com>
2026-02-23 11:40:26 +0700

    game_demo Model

---
+++ game_demo/game.model.js
@@ -0,0 +1,50 @@
+const { ENUM } = require("sequelize")
+const { mapValueFieldNames } = require("sequelize/lib/utils")
+const { getImageUrlById } = require('./game.idurl');
+
+module.exports = (sequelize, DataTypes) => {
+    const games = sequelize.define('games', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true 
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        description:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        discountPrice:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        status:{
+            type: DataTypes.ENUM,
+            value: ['active', 'inactive'],
+            defaultValue: 'inactive'
+        },
+
+        slug: {   
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        }
+        
+    }, {
+        timestamps: true
+    })
+
+    
+    
+
+    
+    return games
+}
--- src/controllers/categorysController.js
+++ src/controllers/categorysController.js
@@ -1,4 +1,4 @@
-const { Users, Categories } = require('../models')
+const { Categories } = require('../models')
 
 
 const createCategory = async (req, res) => {
