3b477dac24da177d7afcec577b6f7314d79d0491
KitsuneNeoxy <1stXPoorinut@gmail.com>
2026-02-25 21:59:17 +0700

    detial update

--- src/models/detail.Model.js
+++ src/models/detail.Model.js
@@ -1,21 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
-    const Order_detail = sequelize.define('or_detail', {
+    const OrderDetail = sequelize.define('OrderDetail', {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
+        },
+        order_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        game_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
-
-
     }, {
         timestamps: true
     })
 
-    Detail.hasMany(GameKey, {
-        foreignKey: 'detail_id',
-        as: 'game_keys'
-    });
+    OrderDetail.associate = (models) => {
+        OrderDetail.belongsTo(models.Order, {
+            foreignKey: 'order_id',
+            as: 'order'
+        })
 
+        OrderDetail.belongsTo(models.Game, {
+            foreignKey: 'game_id',
+            as: 'game'
+        })
+    }
 
-    return Categories
+    return OrderDetail
 }
